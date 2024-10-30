b4e2715f1df0043c7358828b197130393085ed29
AkhiatAdem <abdelrahmanadem1@gmail.com>
2024-10-30 23:02:58 +0100

    saving main page changes

---
+++ owb-frontend/src/components/FloatingDesign.jsx
@@ -0,0 +1,14 @@
+
+
+export default function FloatingDesign(){
+
+    return <div className="absolute w-full h-screen overflow-hidden z-0 hidden md:block">
+   
+    <div className="absolute top-0 left-0 w-24 h-24 bg-[#593E2B] rounded-full opacity-50 animate-float"></div>
+    <div className="absolute top-20 left-10 w-32 h-32 bg-[#6C4D37] rounded-full opacity-30 animate-float"></div>
+    <div className="absolute top-40 left-[50%] w-40 h-40 bg-[#73553F] rounded-full opacity-40 animate-float"></div>
+    <div className="absolute bottom-10 left-20 w-28 h-28 bg-[#5C3D2C] rounded-full opacity-20 animate-float"></div>
+    <div className="absolute bottom-5 left-[35%] w-36 h-36 bg-[#6D4A38] rounded-full opacity-60 animate-float"></div>
+
+  </div>
+}
--- owb-frontend/src/components/Navbar.jsx
+++ owb-frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Link } from "react-router-dom";
+import RegisterButton from "./RegisterButton";
 
 
 
@@ -35,14 +36,14 @@ export default function Navbar() {
                 
         </div>
         <div className="hidden md:flex md:flex-row w-[33vw] justify-center items-center">
-               <button className="w-[22%] border border-[#593E2B] bg-[#593E2B] px-4 py-3 text-[1rem] tracking-wide text-[#CFB290] font-semibold uppercase rounded-lg  hover:bg-[#3d2a1d] transition-all duration-200 cursor-pointer mx-4"> <Link to='/register'>Sign Up</Link></button>
+               <RegisterButton content="Sign Up"/>
                 <button className="w-[22%] border border-[#593E2B] px-4 py-3 text-[1rem] tracking-wide text-[#593E2B] font-semibold uppercase rounded-lg hover:bg-[#957f67] transition-all duration-200 cursor-pointer mx-4"><Link to='/login'>Login</Link></button>
         </div>
-        <div className="absolute top-7 cursor-pointer hover:opacity-70 right-4 md:hidden z-10" onClick={()=>{setMenuOpen(!ismenuOpen)}}>
+        <div className="absolute top-7 cursor-pointer hover:opacity-70 right-4 md:hidden z-30" onClick={()=>{setMenuOpen(!ismenuOpen)}}>
                 <img src={ismenuOpen ? "icons/close.png" : 'icons/menu.png'} className="w-10" alt="" />
                 
         </div>
-        <div className={`md:hidden w-[100vw] h-[100vh] transition-all duration-300 ease-in-out bg-[#CFB290] bg-opacity-70 flex flex-col items-center absolute left-0 top-0 pt-10 gap-7 justify-center z-0 ${ismenuOpen ? 'translate-x-0 ': 'translate-x-[100vw]'} `}>
+        <div className={`md:hidden w-[100vw] h-[100vh] transition-all duration-300 ease-in-out bg-[#CFB290] bg-opacity-70 flex flex-col items-center absolute left-0 top-0 pt-10 gap-7 justify-center z-20 ${ismenuOpen ? 'translate-x-0 ': 'translate-x-[100vw]'} `}>
             <h1 className=" text-[1.1rem] tracking-wide text-[#593E2B] font-semibold uppercase group cursor-pointer hover:opacity-70"  onClick={()=>{setMenuOpen(!ismenuOpen)}}>
                     Men
                     <hr className="w-0 transition-all duration-300 ease-in-out group-hover:w-full border-t-2 border-[#593E2B]"></hr>
