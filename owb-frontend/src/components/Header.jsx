d3067e4dc9696974e9f3595e0c13970f828e26c6
raniamedjadji <raniameriem.medjadji@gmail.com>
2024-11-10 10:26:25 +0100

    header ,navbar

--- owb-frontend/src/components/Header.jsx
+++ owb-frontend/src/components/Header.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.jpeg';
 import Navbar from '../components/Navbar';
+
 const Header = () => {
   return (
-<<<<<<< HEAD
     <header className="bg-beige text-brown p-4 shadow-md">
       <nav className="container mx-auto flex justify-between items-center">
         <Link to="/" className="flex items-center">
           <img src={logo} alt="Logo de la Boutique" className="h-10 mr-2" />
           <span className="text-2xl font-bold">Boutique</span>
         </Link>
-        
       </nav>
-      <NavBar/>
+      <Navbar />
     </header>
-=======
-    <></>
->>>>>>> main
   );
 };
 
