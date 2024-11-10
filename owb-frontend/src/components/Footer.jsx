71e545e8312f6a20e7446fd64636a9427e014765
raniamedjadji <raniameriem.medjadji@gmail.com>
2024-11-10 10:24:01 +0100

    footer

--- owb-frontend/src/components/Footer.jsx
+++ owb-frontend/src/components/Footer.jsx
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
-=======
 import React from 'react';
 
 const Footer = () => {
   return (
-    <></>
+    <footer>
+      <p>&copy; 2024 Votre entreprise. Tous droits réservés.</p>
+      {/* Ajoutez d'autres éléments ici, comme des liens ou des informations supplémentaires */}
+    </footer>
   );
 };
 
 export default Footer;
->>>>>>> main
